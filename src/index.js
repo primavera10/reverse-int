02f2ec944e1d909e3c26fa6d43a902eedf4c4ca6
Liza <lizavetayalak2000@gmail.com>
2023-10-13 20:01:09 +0200

    feat : add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const stringified = n.toString().split('').reverse().join('')
+    return Number.parseInt(stringified, 10)
 }
